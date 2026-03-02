a920ca36cee32c73007a702f506239775274c1a0
Reito0320 <reito@codechrysalis.io>
2026-03-02 19:49:10 +0900

    fix 横列のbingo判定の実装完了 & 中央のpanelの状態を常にtrueに変更

--- client/src/Bingo.jsx
+++ client/src/Bingo.jsx
@@ -4,12 +4,8 @@ import "./Bingo.css";
 export const Bingo = () => {
   const [bingoList, setBingoList] = useState([]);
   const [targetIndex, setTargetIndex] = useState(null);
+  const [bingoCount, setBingoCount] = useState(0);
 
-  const getBingoData = async () => {
-    const response = await fetch("/api/bingo");
-    const allBingoData = await response.json();
-    setBingoList(allBingoData);
-  };
   const changeStatus = (index) => {
     const newArray = [...bingoList];
     newArray[index].status = true;
@@ -22,18 +18,83 @@ export const Bingo = () => {
       headers: { "Content-Type": "application/json" },
     });
   };
-  const bingoReset = async () => {
+  const resetBingo = async () => {
     const res = await fetch("/api/bingo/", {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
     });
     const resetStatusAllData = await res.json();
+    const bingoPanel = resetStatusAllData.find((obj) => obj.title === "Bingo");
+    bingoPanel.status = true;
     setBingoList([...resetStatusAllData]);
+  };
+  const testRowCreator = () => {
+    const allRow = [
+      [0, 1, 2, 3, 4],
+      [5, 6, 7, 8, 9],
+      [10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24],
+    ];
+    return allRow;
+  };
+  const alreadyBingoChecker = (index) => {
+    const allRow = testRowCreator();
+    const find = allRow.find((array) => array.includes(index));
+    const alreadyBingoRow = find.every((index) => bingoList[index].status);
+    return alreadyBingoRow;
+  };
+  const rowChecker = (index) => {
+    const [firstRow, secondeRow, thirdRow, fourthRow, fifthRow] =
+      testRowCreator();
+    if (firstRow.includes(index)) {
+      const firstRowBingo = firstRow.every((index) => bingoList[index].status);
+      if (firstRowBingo) setBingoCount((prev) => prev + 1);
+    }
+    if (secondeRow.includes(index)) {
+      const secondRowBingo = secondeRow.every(
+        (index) => bingoList[index].status,
+      );
+      if (secondRowBingo) setBingoCount((prev) => prev + 1);
+    }
+    if (thirdRow.includes(targetIndex)) {
+      const thirdRowBingo = thirdRow.every((index) => bingoList[index].status);
+      if (thirdRowBingo) setBingoCount((prev) => prev + 1);
+    }
+    if (fourthRow.includes(targetIndex)) {
+      const fourthRowBingo = fourthRow.every(
+        (index) => bingoList[index].status,
+      );
+      if (fourthRowBingo) setBingoCount((prev) => prev + 1);
+    }
+    if (fifthRow.includes(targetIndex)) {
+      const fifthRowBingo = fifthRow.every((index) => bingoList[index].status);
+      if (fifthRowBingo) setBingoCount((prev) => prev + 1);
+    }
+  };
+  const testRowBingo = (index) => {
+    if (targetIndex === null) return;
+    if (alreadyBingoChecker(index)) return;
+    changeStatus(index);
+    patchStatus(index);
+    rowChecker(index);
   };
 
   useEffect(() => {
+    const getBingoData = async () => {
+      const response = await fetch("/api/bingo");
+      const allBingoData = await response.json();
+      const bingoPanel = allBingoData.find((obj) => obj.title === "Bingo");
+      bingoPanel.status = true;
+      setBingoList(allBingoData);
+    };
     getBingoData();
   }, []);
+
+  /* clickが変わるたびに列が揃っているかの精査 */
+  useEffect(() => {
+    testRowBingo(targetIndex);
+  }, [targetIndex]);
 
   return (
     <>
@@ -41,8 +102,6 @@ export const Bingo = () => {
         {bingoList.map((obj, index) => (
           <div
             onClick={() => {
-              changeStatus(index);
-              patchStatus(index);
               setTargetIndex(index);
               console.log(bingoList[index]);
             }}
@@ -50,18 +109,20 @@ export const Bingo = () => {
             className={`bingo-container ${obj.status && "click"}`}
           >
             <img
+              className="bingo-image"
               width={130}
               height={130}
-              src={`/public/${obj.src}.jpg`}
+              src={`/${obj.src}.jpg`}
               alt="画像"
             />
             <p className="bingo-title">{obj.title}</p>
           </div>
         ))}
       </div>
-      <button className="reset-button" onClick={bingoReset}>
+      <button className="reset-button" onClick={resetBingo}>
         bingo reset
       </button>
+      <button onClick={() => console.log(bingoCount)}>point check</button>
     </>
   );
 };
--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,9 +1,12 @@
 const admin = require("firebase-admin");
 // localで動かすとき
-// const serviceAccount = require("./osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
+const env = process.env.NODE_ENV || "./";
+const serviceAccount = require(
+  `${env}osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json`,
+);
 
 //productionで動かすとき
-const serviceAccount = require("/etc/secrets/osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
+// const serviceAccount = require("/etc/secrets/osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
 
 // firebaseAdmin SDK 初期化
 admin.initializeApp({
