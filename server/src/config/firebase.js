a5030eed3b1325c1928cfabe1c896f96a171ed7d
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-02 16:34:54 +0900

    fix: production用にfirebaseシークレットのパスを変更

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -1,5 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
+// localで動かすとき
+// const serviceAccount = require("./osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
+
+//productionで動かすとき
+const serviceAccount = require("/etc/secrets/osampoapp-b5967-firebase-adminsdk-fbsvc-0109fef0ed.json");
 
 // firebaseAdmin SDK 初期化
 admin.initializeApp({
